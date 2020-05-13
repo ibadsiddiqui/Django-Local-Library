290c82066e03e3660f64d3a9b3ad92a02f6ee287
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-05-14 00:42:01 +0500

    --Commit: data display/filter list added for author, book and  instance model

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,35 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+    # Cannot add ManyToManyField genre here
+    list_display = ('title', 'author', 'language', display_genre)
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+
 admin.site.register(Language)
