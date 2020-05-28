d22f17e63cb57625353497caed25dc15aa4b6dea
ibad siddiqui koraspond <ibad@koraspond.app>
2020-05-28 12:12:10 +0500

    --Commit: books list view created

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, BookInstance, Author
+from django.http import Http404
 
 # Create your views here.
+
+
 def index(request):
     """View function for home page of site."""
 
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    
+
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
-    # The 'all()' is implied by default.    
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
-    
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -24,3 +29,33 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+    # your own name for the list as a template variable
+    # context_object_name = 'my_book_list'
+    # queryset = Book.objects.filter(title__icontains='war')[:5]  # Get 5 books containing the title war
+    # # Specify your own template name/location
+    template_name = 'templates/book_list.html'
+
+    def get_queryset(self):
+        #     # Get 5 books containing the title war
+        return Book.objects.filter(title__icontains='war')[:5]
+
+    def get_context_data(self, **kwargs):
+        #     # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+def book_detail_view(request, primary_key):
+    try:
+        book = Book.objects.get(pk=primary_key)
+    except Book.DoesNotExist:
+        raise Http404('Book does not exist')
+
+    return render(request, 'catalog/book_detail.html', context={'book': book})
